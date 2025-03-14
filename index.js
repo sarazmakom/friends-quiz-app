83329cd90432f4774411d36de665a341b6835a2b
Sara Zmak <zmaksara@gmail.com>
2025-03-14 10:28:30 +0100

    add js button on cards functionality

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ document.body.addEventListener("mousemove", (e) => {
   const y = (e.clientY / window.innerHeight) * 100;
   document.body.style.background = `linear-gradient(${x}deg,rgb(157, 69, 212),rgb(239, 137, 166))`;
 });
+
+const cards = document.querySelectorAll("[data-js='card']");
+
+cards.forEach((card) => {
+  const answerButton = card.querySelector("[data-js='answer-button']");
+  const answerDisplay = card.querySelector("[data-js='answer-display']");
+
+  answerButton.addEventListener("click", () => {
+    const isAnswerVisible = answerDisplay.classList.contains(
+      "card__answer--active"
+    );
+
+    if (isAnswerVisible) {
+      answerButton.textContent = "Show answer";
+    } else {
+      answerButton.textContent = "Hide answer";
+    }
+    answerDisplay.classList.toggle("card__answer--active");
+  });
+});
