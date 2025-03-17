71a86ef894705b702d13c9160757fecde0a4f480
Sara Zmak <zmaksara@gmail.com>
2025-03-17 11:13:56 +0100

    add js background to form, add text to input

--- form.js
+++ form.js
@@ -1,3 +1,9 @@
+document.body.addEventListener("mousemove", (e) => {
+  const x = (e.clientX / window.innerWidth) * 100;
+  const y = (e.clientY / window.innerHeight) * 100;
+  document.body.style.background = `linear-gradient(${x}deg,rgb(157, 69, 212),rgb(239, 137, 166))`;
+});
+
 const form = document.querySelector('[data-js="form"]');
 form.addEventListener("submit", (event) => {
   event.preventDefault();
